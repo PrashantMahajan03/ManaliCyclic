1f30aafed3079efd54bd37af8e19b0aea020859e
Prashant Mahajan <prashant.mahajan018@gmail.com>
2022-12-28 22:05:01 +0100

    Removed Production

--- index.js
+++ index.js
@@ -21,12 +21,10 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(express.static(path.join(__dirname, "./client/build")));
 
-if (process.env.NODE_ENV === "production") {
-  app.use("/", express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client/build/index.html"));
-  });
-}
+app.use("/", express.static("client/build"));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client/build/index.html"));
+});
 
 const port = process.env.PORT || 8080;
 app.listen(port, function () {
