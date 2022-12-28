b0e5e5218188da26efe402e25d786ad6d604de1c
Prashant Mahajan <prashant.mahajan018@gmail.com>
2022-12-28 22:57:04 +0100

    Changes as per cyclic docs

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, "./client/build")));
 
 // app.use("/", express.static("client/build"));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 const port = process.env.PORT || 8080;
